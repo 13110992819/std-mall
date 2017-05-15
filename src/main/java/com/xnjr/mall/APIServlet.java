6fe49aa43baec2073fa5ccfebbb74bced89f88f7
pyez1158 <pyez1158@163.com>
2017-05-15 18:55:31 +0800

    版本迭代

--- src/main/java/com/xnjr/mall/APIServlet.java
+++ src/main/java/com/xnjr/mall/APIServlet.java
@@ -42,7 +42,7 @@ public class APIServlet extends HttpServlet {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
         writer.append("Version:3.0.4 \n");
-        writer.append("Description:1th \n");
+        writer.append("Description:1th_ \n");
 
         writer.append("history ******************** \n");
         writer.append("Version:2.0.1 \n");
